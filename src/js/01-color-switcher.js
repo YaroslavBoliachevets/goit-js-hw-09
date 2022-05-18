a0f409799234c5938ff0b22e20379bc8bdcc159f
YaroslavBoliachevets <boliachevets.ya@gmail.com>
2022-05-18 15:28:22 +0300

    ддобавляет настройку и первое задание

---
+++ src/js/01-color-switcher.js
@@ -0,0 +1,44 @@
+const refs = {
+	startBtn: document.querySelector('[data-start]'),
+	stopBtn: document.querySelector('[data-stop]'),
+};
+
+let changeBodyColorInterval = null;
+
+refs.startBtn.addEventListener('click', onStartClick);
+refs.stopBtn.addEventListener('click', onStopClick);
+
+refs.stopBtn.setAttribute("disabled", "true");
+
+
+
+function onStartClick(e) {
+	changeBodyColorInterval = setInterval(() => changeBodyColor(), 1000);
+
+	makeBtnOff(e.target);
+	makeBtnOn(refs.stopBtn);
+}
+
+function onStopClick(e) {
+	clearInterval(changeBodyColorInterval);
+	makeBtnOff(e.target);
+	makeBtnOn(refs.startBtn);
+}
+
+function makeBtnOff(e) {
+	e.setAttribute("disabled", "");
+}
+
+function makeBtnOn(e) {
+	e.removeAttribute("disabled");
+
+}
+
+function changeBodyColor() {
+	document.body.setAttribute("style", getRandomHexColor());
+}
+
+function getRandomHexColor() {
+	return `background-color: #${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
+
